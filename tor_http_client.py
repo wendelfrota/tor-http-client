6a0ec9d5cfa9480f4145ad9d6e6016e67e752054
wendelfrota <wendelalves898@gmail.com>
2024-10-05 14:51:21 -0300

    feat: add post and put methods

--- tor_http_client.py
+++ tor_http_client.py
@@ -37,6 +37,40 @@ class TorHttpClient:
             self.__response = None
         return self.__response
 
+    def post(self, url, data=None, json=None):
+        try:
+            self.__response = requests.post(
+                url=url,
+                data=data,
+                json=json,
+                proxies={
+                    'http': f'socks5://127.0.0.1:{self.__tor_port}',
+                    'https': f'socks5://127.0.0.1:{self.__tor_port}'
+                }
+            )
+            self.__response.raise_for_status()
+        except requests.RequestException as e:
+            print(f'An error occurred: {e}')
+            self.__response = None
+        return self.__response
+
+    def put(self, url, data=None, json=None):
+        try:
+            self.__response = requests.put(
+                url=url,
+                data=data,
+                json=json,
+                proxies={
+                    'http': f'socks5://127.0.0.1:{self.__tor_port}',
+                    'https': f'socks5://127.0.0.1:{self.__tor_port}'
+                }
+            )
+            self.__response.raise_for_status()
+        except requests.RequestException as e:
+            print(f'An error occurred: {e}')
+            self.__response = None
+        return self.__response
+
     def change_ip(self):
         self.__reload_tor()
 
