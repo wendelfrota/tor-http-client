e45546422b3746a723b0c406b95ab10f70c7b5bf
wendelfrota <wendelalves898@gmail.com>
2024-10-08 08:24:11 -0300

    add: delete method

--- tor_http_client.py
+++ tor_http_client.py
@@ -71,6 +71,21 @@ class TorHttpClient:
             self.__response = None
         return self.__response
 
+    def delete(self, url):
+        try:
+            self.__response = requests.delete(
+                url=url,
+                proxies={
+                    'http': f'socks5://127.0.0.1:{self.__tor_port}',
+                    'https': f'socks5://127.0.0.1:{self.__tor_port}'
+                }
+            )
+            self.__response.raise_for_status()
+        except requests.RequestException as e:
+            print(f'An error occurred: {e}')
+            self.__response = None
+        return self.__response
+
     def change_ip(self):
         self.__reload_tor()
 
