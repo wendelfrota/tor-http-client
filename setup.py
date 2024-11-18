12a1d3ea34c823b19282b875efedd4f014ad8163
wendelfrota <wendelalves898@gmail.com>
2024-11-18 16:31:48 -0300

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(cd, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='thc',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
         'requests[socks]'
